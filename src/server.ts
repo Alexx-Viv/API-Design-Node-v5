968deb7bdc18c8a7949dbad38fa3c92a251825fa
Alex Vivsenco <mario.alex.vivsenco@gmail.com>
2025-11-08 16:45:57 +0100

    added validation middleware

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,31 @@
-import express from 'express'
+import express     from 'express'
+
+import authRoutes  from './routes/authRoutes.ts'
+import userRoutes  from './routes/userRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
+
+import { isTest } from '../env.ts'
+
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev', {
+    skip: () => isTest(),
+}))
 
 app.get('/health', (req, res) => {
     res.send('<button>Click me</button>')
 })
 
-app.post('/cake/:name/:id', (req, res) => {
-    res.json(req.params)
-})
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
